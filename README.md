e8e7ee656365ea168c168cdaa88cd3ea0ef06d5e
AudreyHal <chiomahalim@gmail.com>
2019-10-30 09:22:40 -0700

    Change code formatting and add updates

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # Hospital-Inventory
 A platform that allows hospitals to store and access all their patient records online.
 
-Note: You can access prototype version in the link above using the following login details.
+Note: You can access the prototype version in the link above using the following login details.
 Username : admin, Password : admin
+
+## Available Scripts:
+
+In the project directory, you can run:
+
+### npm install
+To install dependencies for the project
+
+### npm start
+Runs the app in the development mode.
+Open http://localhost:3000 to view it in the browser.
+
+
+The demo version of the project connects to an mLab MongoDB database. You can also create your own database on mLab and then add a .env file to the root directory of this project and insert your mongo URI to test run the project:
+
+For example:
+### //.env
+### MONGO_URI= mongodb://<dbuser>:<dbpassword>@ds113849.mlab.com:13849/mydb 
+
--- server.js
+++ server.js
@@ -1,208 +1,295 @@
-const express= require('express');
-const bodyParser= require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const session = require('express-session')
-const app= express();
-const port=process.env.PORT || 3000
-const dotenv=require('dotenv').config({silent:true});
-
+const app = express();
+const port = process.env.PORT || 3000
+const dotenv = require('dotenv').config({
+  silent: true
+});
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({  extended: true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
-
 app.use(session({
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: false }
-  }))
- 
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
+}))
 
 /* MONGOOSE SETUP */
-
 const mongoose = require('mongoose');
 
-var url=process.env.MONGO_URI;
+var url = process.env.MONGO_URI;
 mongoose.connect(url);
 
 const Schema = mongoose.Schema;
 const UserDetail = new Schema({
-      username: String,
-      password: String
-    });
+  username: String,
+  password: String
+});
 const UserDetails = mongoose.model('UserInfo', UserDetail, 'UserInfo');
 
-
 const Patient = new Schema({
-      name: String,
-      date:String,
-      details: String
+  name: String,
+  date: String,
+  details: String
 
-    });
+});
 const Patients = mongoose.model('patient', Patient, 'patient');
 
 
-  app.get('/', function(req, res) {
-    res.render('login',{ alert:"none", msg:"0"})
-  });
- 
-  app.post('/', 
-   function(req, res) {
-    var username= req.body.username 
-    var password= req.body.password
+app.get('/', function (req, res) {
+  res.render('login', {
+    alert: "none",
+    msg: "0"
+  })
+});
 
 
-    if (!req.body.username ||!req.body.password) {
-      res.render('login',{ alert:"block", msg:"Please fill all required fields"})}
+app.post('/',
+  function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
 
-      else{
-    UserDetails.findOne({
-      username: username
-    }, function(err, user) {
-      if (err) 
-      { res.render('login',{ alert:"block", msg:"An error occured. Please try again"});
-
-            } 
-
-      if (!user) {
-        res.render('login',{ alert:"block", msg:"User not found"})
-        
-      }
-
-      if(user){
-
-       if (user.password != password){
-        res.render('login',{ alert:"block", msg:"Incorrect Password"})
-      } else{
-    
-        req.session.user = user;
-        req.session.user.expires = new Date(
-            Date.now() + 3 * 24 * 3600 * 1000); // session expires in 3 days
-           
-          res.redirect('/main');}}
-    });}});
-
- 
-app.use((req, res, next) => {
-      if (req.session.user) {
-      next();
+    if (!req.body.username || !req.body.password) {
+      res.render('login', {
+        alert: "block",
+        msg: "Please fill all required fields"
+      })
     } else {
-     res.redirect("/")
+      UserDetails.findOne({
+        username: username
+      }, function (err, user) {
+        if (err) {
+          res.render('login', {
+            alert: "block",
+            msg: "An error occured. Please try again"
+          });
+
+        }
+
+        if (!user) {
+          res.render('login', {
+            alert: "block",
+            msg: "User not found"
+          })
+
+        }
+
+        if (user) {
+
+          if (user.password != password) {
+            res.render('login', {
+              alert: "block",
+              msg: "Incorrect Password"
+            })
+          } else {
+
+            req.session.user = user;
+            req.session.user.expires = new Date(
+              Date.now() + 3 * 24 * 3600 * 1000); // session expires in 3 days
+
+            res.redirect('/main');
+          }
+        }
+      });
     }
   });
 
-  app.get('/main',function(req,res){
- var name=[];
-name.push(req.session.user.username);
-    
-    res.render('index', {names:name[0]} );
- 
+
+app.use((req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/")
+  }
+});
+
+
+app.get('/main', function (req, res) {
+  var name = [];
+  name.push(req.session.user.username);
+  res.render('index', {
+    names: name[0]
+  });
+});
+
+
+app.get('/find', function (req, res) {
+  res.render('find', {
+    alert: "none",
+    msg: ""
+  });
+});
+
+
+app.get('/add', function (req, res) {
+  res.render('add', {
+    alert: "none",
+    msg: "",
+    status: "success"
+  });
+});
+
+
+app.get('/details', function (req, res) {
+  res.render('details');
+});
+
+
+app.get('/delete', function (req, res) {
+  res.render('delete', {
+    alert: "none",
+    msg: "",
+    status: "success"
+  });
+});
+
+
+app.post('/delete', function (req, res) {
+  if (req.body.firstname == "" || req.body.lastname == "") {
+    res.render('delete', {
+      msg: "Please fill in empty fields",
+      alert: "block",
+      status: "danger"
+    })
+  } else {
+    var name = req.body.firstname + " " + req.body.lastname;
+    var fullname = name.toUpperCase();
+    Patients.find({
+      name: fullname
+    }, function (err, data) {
+      if (err)
+
+      {
+        res.render('delete', {
+          msg: 'An Error Occured',
+          alert: "block",
+          status: "danger"
+        })
+      }
+      if (!data.length) {
+        res.render('delete', {
+          msg: 'No account with that name exists.',
+          alert: "block",
+          status: "danger"
+        });
+      }
+      if (data) {
+        res.render('confirmation', {
+          data: data
+        });
+      }
+    })
+  }
+});
+
+
+app.post('/confirmation', function (req, res) {
+  var name = req.body.name;
+
+  Patients.deleteMany({
+    name: name
+  }, function (err, data) {
+    if (err) {
+      res.send('try again later')
+    }
+    if (data) {
+      res.render('delete', {
+        msg: 'One record deleted.',
+        alert: "block",
+        status: "success"
+      });
+    }
+  })
+});
+
+
+app.post('/search', function (req, res) {
+  if (req.body.firstname == "" || req.body.lastname == "") {
+    res.render('find', {
+      msg: "Please fill in empty fields",
+      alert: "block"
+    })
+  } else {
+    var name = req.body.firstname + " " + req.body.lastname;
+    var fullname = name.toUpperCase();
+    Patients.find({
+      name: fullname
+    }, function (err, data) {
+      if (err) {
+        res.send('try again later')
+      }
+      if (!data.length) {
+        res.render('find', {
+          msg: 'No account with that name exists.',
+          alert: "block"
+        });
+      } else {        
+        res.render('details', {
+          data: data
+        });
+      }
+    })
+  }
+});
+
+
+app.post('/add', function (req, res) {
+  if (req.body.firstname == "" || req.body.lastname == "" || req.body.date == "" || req.body.details == "") {
+    res.render('add', {
+      msg: "Please fill in empty fields",
+      alert: "block",
+      status: "danger"
+    })
+  } else {
+    var name = req.body.firstname + " " + req.body.lastname;
+    var fullname = name.toUpperCase();
+
+    const patient = new Patients({
+      name: fullname,
+      date: req.body.date,
+      details: req.body.details
     });
 
-  app.get('/find', function(req,res){ 
-    res.render('find',{ alert:"none", msg:""});});
-
-    app.get('/add', function(req,res){
-      res.render('add',{ alert:"none", msg:"",status:"success"});});
-  
-
-    app.get('/details', function(req,res){
-        res.render('details');});
-
-     
-          
-            app.get('/delete', function(req,res){
-              res.render('delete',{ alert:"none", msg:"",status:"success"});});
-
-            
-
-              app.post('/delete', function(req,res){
-                if (req.body.firstname =="" || req.body.lastname =="" ){ res.render('delete',{msg:"Please fill in empty fields", alert:"block",status:"danger"})}
-               else{
-                 var name=req.body.firstname +" " + req.body.lastname ;
-               var fullname = name.toUpperCase();
-                       Patients.find({name:fullname},function(err,data) {
-                         if(err)
-                         
-                         {res.render('delete',{msg:'An Error Occured', alert:"block", status:"danger"})}
-                          if (!data.length){res.render('delete',{msg:'No account with that name exists.', alert:"block", status:"danger"});}
-                       if(data){
-                         res.render('confirmation',{data:data});
-                       }
-                        
-                 })
-               }
-               });
-               
-               app.post('/confirmation', function(req,res){
-                var name=req.body.name;
-                
-                  Patients.deleteMany({name:name},function(err,data) {
-                         if(err){res.send('try again later')}
-                        if(data)  {
-                         res.render('delete',{msg:'One record deleted.', alert:"block", status:"success"});
-                       }
-                        
-                 })
-               
-               });
-    
-
-  app.post('/search', function(req,res){
- if (req.body.firstname =="" || req.body.lastname =="" ){ res.render('find',{msg:"Please fill in empty fields", alert:"block"})}
-else{
-  var name=req.body.firstname +" " + req.body.lastname ;
-var fullname = name.toUpperCase();
-        Patients.find({name:fullname},function(err,data) {
-          if(err){res.send('try again later')}
-        if (!data.length) {res.render('find',{msg:'No account with that name exists.', alert:"block"});}
-        else {
-          console.log(data); 
-          res.render('details',{data:data});
-        }
-         
-  })
-}
-});
- 
-
-app.post('/add', function(req,res){
-  if (req.body.firstname =="" || req.body.lastname =="" || req.body.date =="" || req.body.details =="" ){ 
-    res.render('add',{msg:"Please fill in empty fields", alert:"block",status:"danger"})} 
-    else{
-var name=req.body.firstname +" " + req.body.lastname ;
-var fullname = name.toUpperCase();
-
-const patient = new Patients({
-    name: fullname ,
-    date:req.body.date,
-    details: req.body.details});
-
-
-patient.save()
-.then((data)=> {
-    res.render('add',{msg:"Saved to database", alert:"block", status:"success"})
- })
-.catch((err)=> {
- res.send('add',{msg:"An error Occured", alert:"block", status:"danger"});
-})}});
-
-app.get('/logout',function(req,res){
-  if(req.session){
-      //delete session object
-      req.session.destroy(function(err){
-          if(err){res.send("An error occured");}
-          else{
-              return res.redirect('/')
-          }
+    patient.save()
+      .then((data) => {
+        res.render('add', {
+          msg: "Saved to database",
+          alert: "block",
+          status: "success"
+        })
+      })
+      .catch((err) => {
+        res.send('add', {
+          msg: "An error Occured",
+          alert: "block",
+          status: "danger"
+        });
       })
   }
 });
-app.listen(port, ()=>{
-  console.log('Server initiated succesfully');
-   
-    
+
+
+app.get('/logout', function (req, res) {
+  if (req.session) {
+    //delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        res.send("An error occured");
+      } else {
+        return res.redirect('/')
+      }
+    })
+  }
 });
 
-                  
+
+app.listen(port, () => {
+  console.log('Server initiated succesfully on port ' + port);
+
+});
